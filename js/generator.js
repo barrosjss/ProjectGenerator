6f9fbc89c3f9127d9af21005a7e0531aea8adf0b
tract0rb <jesus.reinaldo.barros@gmail.com>
2023-10-04 12:07:52 -0500

    change::generator.js

--- js/generator.js
+++ js/generator.js
@@ -1,16 +1,21 @@
 const options = [
-    "Opción 1",
-    "Opción 2",
-    "Opción 3",
-    "Opción 4",
-    "Opción 5"
+  ["Ableton", "https://www.frontendpractice.com/projects/ableton"],
+  ["Bonsai", "https://www.frontendpractice.com/projects/bonsai"],
+  ["Pulse", "https://www.frontendpractice.com/projects/pulse"],
+  ["Podia", "https://www.frontendpractice.com/projects/podia"],
+  ["Starbucks", "https://www.frontendpractice.com/projects/starbucks"],
+  ["Ecwid", "https://www.frontendpractice.com/retired/ecwid"],
+  ["Grasshopper", "https://www.frontendpractice.com/retired/grasshopper"],
+  ["Red Square", "https://www.frontendpractice.com/retired/red-square"],
+  ["Instrument", "https://www.frontendpractice.com/retired/instrument"],
+  ["Shopify Dev", "https://www.frontendpractice.com/retired/shopify-dev"],
 ];
 
 const resultDiv = document.getElementById("result");
 const generateBtn = document.getElementById("generateBtn");
 
 generateBtn.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * options.length);
-    const randomOption = options[randomIndex];
-    resultDiv.textContent = `Opción seleccionada: ${randomOption}`;
+  const randomIndex = Math.floor(Math.random() * options.length);
+  const [optionName, optionLink] = options[randomIndex];
+  resultDiv.innerHTML = `<p>Opción seleccionada: <a href="${optionLink}" target="_blank">${optionName}</a></p>`;
 });
